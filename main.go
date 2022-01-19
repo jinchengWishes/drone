7d9cc0a64dce661b91f37dd1a4fe689b84eda8c9
jinchengli <2313988763@qq.com>
2022-01-19 15:53:43 +0800

    7

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("1234567890")
+	fmt.Println("启动服务成功...")
+	http.HandleFunc("/", sayHello)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		fmt.Printf("http server failed, err:%v\n", err)
+		return
+	}
+}
+
+// http server
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, map[interface{}]interface{}{
+		"name": "jincheng",
+		"age":  "20",
+	})
 }
