f8818b93187a34c833d53b5d7196ab38104559c3
jinchengli <2313988763@qq.com>
2022-03-08 11:53:05 +0800

    bb

--- main.go
+++ main.go
@@ -2,24 +2,45 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+
+	"github.com/casbin/casbin/v2"
+	"github.com/kotlin2018/adapter"
 )
 
 func main() {
-	fmt.Println("启动服务成功...")
-	http.HandleFunc("/", sayHello)
-	err := http.ListenAndServe(":9090", nil)
-	if err != nil {
-		fmt.Printf("http server failed, err:%v\n", err)
-		return
+	opts := &adapter.Adapter{
+		DriverName: "mysql",
+		LinkInfo:   "root:root@tcp(127.0.0.1:3306)/golang",
+		TableName:  "casbin_rule",
 	}
+
+	c := &adapter.CasBinModel{
+		BaseAdapter: opts,
+		ModelPath:   "./rbac_model.conf",
+	}
+
+	e, err := casbin.NewEnforcer(c, opts)
+	if err != nil {
+		fmt.Println("err:", err)
+	}
+	fmt.Println("ok:", e)
+
+	// boolean, err := e.Enforce("100", "/v1/post/info", "post")
+	// if err != nil {
+	// 	return
+	// }
+	// if boolean {
+	// 	fmt.Println("有权限")
+	// } else {
+	// 	fmt.Println("没有权限")
+	// }
 }
 
 // http server
 
-func sayHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, map[interface{}]interface{}{
-		"name": "jincheng",
-		"age":  "20",
-	})
-}
+// func sayHello(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintln(w, map[interface{}]interface{}{
+// 		"name": "jincheng",
+// 		"age":  "20",
+// 	})
+// }
